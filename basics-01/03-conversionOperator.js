cc14cbe7b96e7bf572c6d25472983f927a6ecbc3
kjgourav <kjgourav@gmail.com>
2025-01-31 10:13:16 +0000

    ConversionofDatatypes

---
+++ basics-01/03-conversionOperator.js
@@ -0,0 +1,27 @@
+let score = "hitesh" //first the valur was string
+
+console.log(typeof(score));//so it will print the type of score which was string 
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);//nan = not a number but the datatype is number
+
+
+//"33" => 33
+//"33abc" => NaN 
+// ture => 1 : false => 0
+
+let isLoggedIn  = "gourav"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true ; 0=> false
+//  ""=> false
+//  "hitesh" => true
+
+let someNum = 33
+
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof stringNum);
