4c9d926bceacffe2996de05bf2d3980b9e20cba7
kjgourav <kjgourav@gmail.com>
2025-01-31 09:51:49 +0000

    datatypes

---
+++ basics-01/02-datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) // we are using nodejs not browser
+
+//console.log(3+
+  //  3) //readability should be high
+
+ let name = "gourav"
+ let num = 32
+
+//  number => 2 to the power 53
+// bignit
+// string => ""
+// boolean => t/f
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// non primitive
+// object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
