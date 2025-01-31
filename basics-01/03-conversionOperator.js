f6db6c48a75f9274bdff58c219f84cb885263bc5
kjgourav <kjgourav@gmail.com>
2025-01-31 10:54:22 +0000

    conversionoperator

--- basics-01/03-conversionOperator.js
+++ basics-01/03-conversionOperator.js
@@ -1,10 +1,10 @@
 let score = "hitesh" //first the valur was string
 
-console.log(typeof(score));//so it will print the type of score which was string 
+// console.log(typeof(score));//so it will print the type of score which was string 
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);//nan = not a number but the datatype is number
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);//nan = not a number but the datatype is number
 
 
 //"33" => 33
@@ -14,7 +14,7 @@ console.log(valueInNumber);//nan = not a number but the datatype is number
 let isLoggedIn  = "gourav"
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 1 => true ; 0=> false
 //  ""=> false
@@ -23,5 +23,40 @@ console.log(booleanisLoggedIn);
 let someNum = 33
 
 let stringNum = String(someNum)
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+// **************************** operations *********
+
+let  value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1+ str2
+// console.log(str3);
+
+// console.log("1"+2);// 12
+// console.log(1+"2" + 2);//122
+// console.log("1" +2 + 2);//122
+// console.log(1 +2 +"2" + 2);//322
+// after something is string automatically teh next value will be converted to string
+//but in the 4th one furst two letters are no so theya re added up like noraml and after that its string so the number after the string was also taken as string
+
+// console.log(+true);//1
+// console.log(+"");//0
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
