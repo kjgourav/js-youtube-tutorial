039b2ceedc11380fcfeb50fcc2ded8949806da9b
kjgourav <kjgourav@gmail.com>
2025-02-01 16:24:09 +0000

    daatbase

--- basics-01/datatypes-summary.js
+++ basics-01/datatypes-summary.js
@@ -8,10 +8,17 @@ const scoreValue = 100.3
 const isLoggedIn = false
 const outsideTemp = null
 
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);//false
+//both their return type is diff
 
 // referenced type (non primitive)
 
 // array,objects,functions
+const heros = ["shaktimaan", "nagraj", "doga"]//array
+ 
 
 
 
