474352fd90a6499db0d337645cdb6475d8da93f9
kjgourav <kjgourav@gmail.com>
2025-02-02 15:58:50 +0530

    conversion

--- basics-01/03-conversionOperator.js
+++ basics-01/03-conversionOperator.js
@@ -46,7 +46,7 @@ let str2 = " hitesh"
 let str3 = str1+ str2
 // console.log(str3);
 
-// console.log("1"+2);// 12
+// console.log("1"+2);// 11
 // console.log(1+"2" + 2);//122
 // console.log("1" +2 + 2);//122
 // console.log(1 +2 +"2" + 2);//322
