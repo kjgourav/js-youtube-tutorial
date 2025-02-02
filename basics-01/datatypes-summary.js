7c31d5411cd32f79d64642e57eb60c72cb489c7f
kjgourav <kjgourav@gmail.com>
2025-02-02 15:58:09 +0530

    datatypes

--- basics-01/datatypes-summary.js
+++ basics-01/datatypes-summary.js
@@ -18,7 +18,26 @@ console.log(id === anotherId);//false
 
 // array,objects,functions
 const heros = ["shaktimaan", "nagraj", "doga"]//array
+
+let obj1 = {
+    x :10,
+    name : { city : "mumbai"}
+}
+
+let obj2 = obj1 
+
+obj2.x=20
+
+console.log(obj1); // x:20 because non primitive store refernecs to orinial value so change in obj2 is reflected in obj1
  
+let obj3 = {...obj1}
+
+obj3.x=12
+console.log(obj1); // the x value in obj1 will not change because ...obj1 created a shallow copy of it in obj3 and because the primitive types 
+//store direct value and if try to acces it the copy of the value will be accesed
+//but if we try to change the nested object value thriugh ibj3 it will change because it stores reference and the reference will be copied of the original value
+
+
 
 
 
